9021a5fac1759a5ac02a9190da25df5f76c1ed4c
SagiMarton <sagi.marton1@gmail.com>
2023-12-01 11:15:42 +0100

    calculateCalibrationValueSum implementation

--- 2023/Trebuchet/main.cpp
+++ 2023/Trebuchet/main.cpp
@@ -5,7 +5,6 @@ int main() {
     std::ifstream *input = new std::ifstream("input.txt", std::ifstream::in);
     int result = calculateCalibrationValueSum(input);
 
-
     std::cout << result << std::endl;
     input->close();
     delete input;
@@ -13,4 +12,13 @@ int main() {
     return 0;
 }
 
+int calculateCalibrationValueSum(std::ifstream* input){
+    int sum = 0;
+    std::string* line = new std::string();
+    while (std::getline(*input,*line)){
+        sum += getCalibrationValueFromLine(line);
+    }
+    
+    return sum;
+}
 
